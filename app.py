6828a48ec07493a78fb1f842bb009c5c83721991
soyvitoupro <vitoupro.vtp@gmail.com>
2024-09-14 14:40:53 +0700

    update app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 app = Flask(__name__)
 model = joblib.load('random_forest_model.pkl')
 
-@app.route('/')
+@app.route('/homepage')
 def home():
     return render_template('home.html')
 
@@ -21,4 +21,4 @@ def serve_flower(filename):
     return send_from_directory('flower', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=4980) # start port 4980 
